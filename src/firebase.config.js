cd01b57ccc16ad60abd2f468306d4340adbbc6d7
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-01-07 13:45:18 +0530

    house marketing project

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCGdkLiBH_oNxW3f3nnq-Wnw4kpz89uKo4",
+  authDomain: "house-marketplace-app-a609d.firebaseapp.com",
+  projectId: "house-marketplace-app-a609d",
+  storageBucket: "house-marketplace-app-a609d.appspot.com",
+  messagingSenderId: "851853102730",
+  appId: "1:851853102730:web:8d7e635571a3be86cf2773"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore()
