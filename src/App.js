c066461c569ab6d1192d2c763bbf49dcc1e4cd86
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-01-07 16:17:49 +0530

    routing part and half navigation part done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import ForgotPassword from "./pages/ForgotPassword";
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore/>}/>
+          <Route path="/sign-up" element={<SignUp/>}/>
+          <Route path="/sign-in" element={<SignIn/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/offers" element={<Offers/>}/>
+          <Route path="/forgot-password" element={<ForgotPassword/>}/>
+          <Route path="/explore" element={<Explore/>}/>
+        </Routes>
+          <Navbar/>
+      </Router>
     </>
   );
 }
