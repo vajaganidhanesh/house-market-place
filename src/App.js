17c14625f282ed82243e1bffb5a0a267fa38407b
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-01-10 14:51:22 +0530

    listing page userInterface has been updated successfully

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
 import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
           <Route path="/forgot-password" element={<ForgotPassword/>}/>
           <Route path="/create-listing" element={<CreateListing />}/>
           <Route path="/explore" element={<Explore/>}/>
+          <Route path='/category/:categoryName/:listingId' element={<Listing/>}/>
         </Routes>
           <Navbar/>
       </Router>
