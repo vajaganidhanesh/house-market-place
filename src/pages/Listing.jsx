909fce13ace1f25b298c19ca09c588ca23e9f0c5
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-01-12 15:34:01 +0530

    curosal part done with in listing component

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,13 +1,15 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import SwiperCore,{Navigation,Pagination,Scrollbar,Ally} from 'swiper';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+import 'swiper/swiper-bundle.css'
 import {Swiper,SwiperSlide} from 'swiper/react'
 import { getDoc, doc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { db } from "../firebase.config";
 import Spinner from "../components/Spinner";
 import shareIcon from "../assets/svg/shareIcon.svg";
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 function Listing() {
   const [listing, setListing] = useState(null);
