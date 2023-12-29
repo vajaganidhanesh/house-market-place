57736f254970e65a8f9c2a1cca8e4afd125bbdae
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-12-29 12:33:14 +0530

    Code splitting technique to increase performance

--- src/App.js
+++ src/App.js
@@ -1,46 +1,52 @@
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import Explore from "./pages/Explore";
-import SignUp from "./pages/SignUp";
-import SignIn from "./pages/SignIn";
 import Profile from "./pages/Profile";
 import Offers from "./pages/Offers";
-import ForgotPassword from "./pages/ForgotPassword";
 import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
 import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
-import EditListing from "./pages/EditListing";
-import Listing from "./pages/Listing";
-import Contact from "./pages/Contact";
+import Spinner from "./components/Spinner";
+
+const SignIn = lazy(() => import("./pages/SignIn"));
+const SignUp = lazy(() => import("./pages/SignUp"));
+const Listing = lazy(() => import("./pages/Listing"));
+const Contact = lazy(() => import("./pages/Contact"));
+const ForgotPassword = lazy(() => import("./pages/ForgotPassword"));
+const EditListing = lazy(() => import("./pages/EditListing"));
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Explore/>}/>
-          <Route path="/sign-up" element={<SignUp/>}/>
-          <Route path="/sign-in" element={<SignIn/>}/>
-          
-          <Route path='/profile' element={<PrivateRoute/>} >
-            <Route path="/profile" element={<Profile/>}/>
-          </Route>
+      <Suspense fallback={<Spinner />}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Explore />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/sign-in" element={<SignIn />} />
 
-          <Route path="/offers" element={<Offers/>}/>
-          <Route path='/category/:categoryName' element={<Category/>}/>
-          <Route path="/forgot-password" element={<ForgotPassword/>}/>
-          <Route path="/create-listing" element={<CreateListing />}/>
-          <Route path="/edit-listing/:listingId" element={<EditListing />}/>
-          <Route path="/contact/:landlordId" element={<Contact />}/>
-          <Route path="/explore" element={<Explore/>}/>
-          <Route path='/category/:categoryName/:listingId' element={<Listing/>}/>
-        </Routes>
-          <Navbar/>
-      </Router>
-      
-      <ToastContainer/>
+            <Route path="/profile" element={<PrivateRoute />}>
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+            <Route path="/offers" element={<Offers />} />
+            <Route path="/category/:categoryName" element={<Category />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/create-listing" element={<CreateListing />} />
+            <Route path="/edit-listing/:listingId" element={<EditListing />} />
+            <Route path="/contact/:landlordId" element={<Contact />} />
+            <Route path="/explore" element={<Explore />} />
+            <Route
+              path="/category/:categoryName/:listingId"
+              element={<Listing />}
+            />
+          </Routes>
+          <Navbar />
+        </Router>
+        <ToastContainer />
+      </Suspense>
     </>
   );
 }
